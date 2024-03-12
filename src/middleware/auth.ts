b90eb8373a951e966c5e046c644ed0d9dd3aa6df
kariithi-samuel <samuelmainak70@gmail.com>
2024-03-12 15:39:55 +0300

    adding user profile features

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,13 +1,13 @@
-import { Request,Response,NextFunction } from "express";
-import { auth } from "express-oauth2-jwt-bearer";
-import  jwt  from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 import User from "../models/user";
+import { auth } from "express-oauth2-jwt-bearer";
 
-declare global{
-  namespace Express { 
-    interface Request{
-      userId: String;
-      auth0Id: String;
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+      auth0Id: string;
     }
   }
 }
@@ -15,22 +15,22 @@ declare global{
 export const jwtCheck = auth({
   audience: process.env.AUTH0_AUDIENCE,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
-  tokenSigningAlg: 'RS256'
+  tokenSigningAlg: "RS256",
 });
 
-export const jwtParse = async (req: Request,
+export const jwtParse = async (
+  req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
     return res.sendStatus(401);
   }
 
+  // Bearer lshdflshdjkhvjkshdjkvh34h5k3h54jkh
   const token = authorization.split(" ")[1];
-
 
   try {
     const decoded = jwt.decode(token) as jwt.JwtPayload;
@@ -40,12 +40,12 @@ export const jwtParse = async (req: Request,
 
     if (!user) {
       return res.sendStatus(401);
-}
-    req.auth0Id = auth0Id as String;
-    req.userId = user?._id?.toString() || "";
+    }
+
+    req.auth0Id = auth0Id as string;
+   req.userId = user._id!.toString(); 
     next();
   } catch (error) {
-    return res.sendStatus(401)
+    return res.sendStatus(401);
   }
-
 };
